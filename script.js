034c062f6c1c593cc4cabaa25468281e9c8cca78
zeekkhan <azizkhanusllc@gmail.com>
2024-03-31 22:58:02 +0500

    Done task

---
+++ script.js
@@ -0,0 +1,85 @@
+// const inputBox = document.getElementById("input-box");
+// const listContainer = document.getElementById("list-container");
+
+// function addtask(){
+//     if (inputBox.value === ""){
+//         alert("Please enter a task");
+//     }
+//     else{
+//         let li = document.createElement("li");
+//         li.innerHTML = inputBox.value;
+//         listContainer.appendChild(li);
+//         let span = document.createElement("span");
+//         span.innerHTML = "Remove";
+//         li.appendChild(span);
+//     }
+//     inputBox.value = "";
+//     saveData();
+// }
+
+
+// listContainer.addEventListener("click", function(e) {
+//   if (e.target.tagName === "LI") {
+//     e.target.classList.toggle("checked");
+//     saveData();
+//   } else if (e.target.tagName === "SPAN") {
+//     e.target.parentElement.remove();
+//     saveData();
+//   }
+// }.bind(this));
+
+// function saveData(){
+//     localStorage.setItem("data", listContainer.innerHTML);
+// }
+
+// function loadData(){
+//     listContainer.innerHTML = localStorage.getItem("data");
+// }
+// loadData();
+
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addtask(){
+  if (inputBox.value === ""){
+      alert("Please enter a task");
+  }
+  else{
+      let li = document.createElement("li");
+      li.innerHTML = inputBox.value;
+      li.contentEditable = "true"; // Allow the user to edit the task
+      listContainer.appendChild(li);
+      let span = document.createElement("span");
+      span.innerHTML = "Remove";
+      li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+if (e.target.tagName === "LI") {
+  e.target.classList.toggle("checked");
+  saveData();
+} else if (e.target.tagName === "SPAN") {
+  e.target.parentElement.remove();
+  saveData();
+}
+}.bind(this));
+
+// Save changes automatically when the user finishes editing a task
+listContainer.addEventListener("input", function(e) {
+if (e.target.tagName === "LI") {
+  saveData();
+}
+}.bind(this));
+
+function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function loadData(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+loadData();
